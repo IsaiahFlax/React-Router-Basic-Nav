eb55968619cd4040e0161871c3978413893a145b
IsaiahFlax <flax.isaiah@gmail.com>
2020-02-11 19:38:31 -0700

    startaing navigation section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
+    <Route exact path="/" component = {Home}>Home</Route>
+    <Route path="/about" component = {About}>About</Route>
+    <Route path="/contact" component = {Contact}>Contact</Route>
     <Navigation />
   </div>
 );
